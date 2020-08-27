9ec455bcd14029b6a9f8ce0aba50af5ff108072a
Mostafa Alakdawi <mostafa.alakdawi@outlook.com>
2020-08-27 12:19:33 +0200

    Adding the implementation of parseInts function file for problem StringStream and finalizing the solution for the problem

--- C++/StringStream/StringStream.cpp
+++ C++/StringStream/StringStream.cpp
@@ -2,10 +2,30 @@
 #include <vector>
 #include <iostream>
 using namespace std;
+vector<int> parseInts(string str);
 
+/*
+ *	function: parseInts
+ *	Exracts numerical data from strings and returns 
+ *	them as a vector of intgers
+ */
 vector<int> parseInts(string str) {
-	// Complete this function
+    int start = 0;
+    int end = 0;
+    vector<int> numbers;
+
+    for(int count = 0; count < str.length()+1; count++){
+        
+        if(str[count] == ',' || count == str.length()){
+            end = count;
+            numbers.push_back(stoi(str.substr(start,(end-start))));
+            start = end+1;
+        }
+    }
+
+    return numbers;
 }
+
 
 int main() {
     string str;
