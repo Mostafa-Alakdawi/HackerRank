bcce95759fc631da3d73819432ff173c7dfd779e
Mostafa Alakdawi <mostafa.alakdawi@outlook.com>
2020-09-18 23:54:41 +0200

    Adding the implementation of hourglassSum() function and finalizing he solution\ for the |2D Array - DS| coding proble

--- Problem_Solving/Data_Structures/2D Arrays-DS/Array-DS.cpp
+++ Problem_Solving/Data_Structures/2D Arrays-DS/Array-DS.cpp
@@ -4,8 +4,27 @@ using namespace std;
 
 // Complete the hourglassSum function below.
 int hourglassSum(vector<vector<int>> arr) {
+    int row = 0, col = 0;
+    int max_sum = -100, current_sum = 0;
 
+    while(row < 4 && col < 4){
+        
+        current_sum = arr[row][col] + arr[row][col+1] + arr[row][col+2] + arr[row+1][col         +1] + arr[row+2][col] + arr[row+2][col+1] + arr[row+2][col+2];
 
+        if(current_sum > max_sum){
+            max_sum = current_sum;
+        }
+        
+        if(col == 3){
+            row++;
+            col = 0;
+        }
+        else{
+            col++;
+        }
+    }
+
+    return max_sum;
 }
 
 int main()
