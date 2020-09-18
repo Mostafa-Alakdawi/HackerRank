0c905791d9377aca02239d2bb8860b4999de323b
Mostafa Alakdawi <mostafa.alakdawi@outlook.com>
2020-09-19 00:00:44 +0200

    Adding the implementation of max_of_four function() and finalizing the solution for the |Functions in C| coding problem

--- C/Functions in C/Functions_in_C.c
+++ C/Functions in C/Functions_in_C.c
@@ -1,10 +1,19 @@
-#include <iostream>
-#include <cstdio>
-using namespace std;
-
+#include <stdio.h>
 /*
 Add `int max_of_four(int a, int b, int c, int d)` here.
 */
+int max_of_four(int a, int b, int c, int d){
+    int max = a;
+
+    if(max < b)
+        max = b;
+    if(max < c)
+        max = c;
+    if(max < d)
+        max = d;
+    
+    return max;
+}
 
 int main() {
     int a, b, c, d;
