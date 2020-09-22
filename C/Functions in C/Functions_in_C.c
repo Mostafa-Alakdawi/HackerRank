0b05304b8cf048ebfabfb19b74fb56393c929968
Mostafa Alakdawi <mostafa.alakdawi@outlook.com>
2020-09-22 22:11:31 +0200

    Adding comments to max_of_four() function

--- C/Functions in C/Functions_in_C.c
+++ C/Functions in C/Functions_in_C.c
@@ -1,7 +1,28 @@
 #include <stdio.h>
+
 /*
 Add `int max_of_four(int a, int b, int c, int d)` here.
 */
+
+/*
+ *	Function name: 
+ *	max_of_four
+ *
+ *	Parameters:
+ *	a: an integer that represents the first number
+ *	b: an integer that represents the second number
+ *	c: an integer that represents the third number
+ *	d: an integer that represents the fourth number
+ *
+ *	return:
+ *	an integer that represents the maximum of the four numbers
+ *
+ *
+ *	Function description:	 
+ *	Compares the four numbers with each other and returns the 
+ *	maximum number among them  
+ *
+ */
 int max_of_four(int a, int b, int c, int d){
     int max = a;
 
