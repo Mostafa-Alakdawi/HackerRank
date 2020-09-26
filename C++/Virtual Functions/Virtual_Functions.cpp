2691ad067b35d8344c5ee460cdb43d593a7eca72
Mostafa Alakdawi <mostafa.alakdawi@outlook.com>
2020-09-26 14:29:28 +0200

    Adding the following implementations: class Person
    Public member variable and functions
    - string name
    - int age
    - virtual void getdata()
    - virtual void putdata()
    
    class Professor
    
    Private member variables
    - int publications
    - static int cur_id
    - int id
    
    Construtor:
    - Professor()
    
    Public member functions:
    - void getdata()
    - void putdata()
    
    class Student
    
    private member variable
    - int publications
    - vector<int> marks
    - static int cur_id
    - int id;
    
    Constructor:
    - Student()
    
    Public member functions:
    - int marksSum()
    - void getdata()
    - void putdata()

--- C++/Virtual Functions/Virtual_Functions.cpp
+++ C++/Virtual Functions/Virtual_Functions.cpp
@@ -5,6 +5,81 @@
 #include <algorithm>
 using namespace std;
 
+class Person{
+    public:
+        string name;
+        int age;
+        virtual void getdata() = 0;
+        virtual void putdata() = 0;
+};
+
+
+class Professor:public Person{
+    private:
+        int publications;
+        static int cur_id;
+        int id;
+
+    public:
+        Professor():id{cur_id++}{
+            //std::cout << "Hi"<<std::endl;
+        }
+        void getdata(){
+            //std::cout << "Enter your name: "<< std::endl;
+            std::cin >> this->name;
+            //std::cout << "Enter your name: "<< std::endl;
+            std::cin >> this->age;
+            std::cin >> this->publications;
+        }
+        void putdata(){
+            std::cout << this->name << " ";
+            std::cout << this->age << " ";
+            std::cout << this->publications << " ";
+            std::cout << this->id << std::endl;
+        }
+};
+
+class Student:public Person{
+    private:
+        int publications;
+        vector<int> marks;
+        static int cur_id;
+        int id;
+
+    public:
+        Student():id{cur_id++}{
+            marks = vector<int>(6,0);
+            //cur_id++;
+        }
+        
+        int marksSum(){
+            int sum = 0;
+
+            for(int index = 0; index < 6; index++){
+                sum += marks[index];
+            }
+
+            return sum;
+        }
+
+        void getdata(){
+            std::cin >> this->name;
+            std::cin >> this->age;
+
+            for(int index = 0; index < 6; index++)
+                std::cin >> this->marks[index];
+        }
+
+        void putdata(){
+            std::cout << this->name << " ";
+            std::cout << this->age << " ";
+            std::cout << this->marksSum() << " ";
+            std::cout << this->id << std::endl;
+        }
+};
+
+int Professor::cur_id {1};
+int Student::cur_id {1};
 int main(){
 
     int n, val;
