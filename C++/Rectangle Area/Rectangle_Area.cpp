59756a0105abca14705db371b270d1a60bc4a893
Mostafa Alakdawi <mostafa.alakdawi@outlook.com>
2020-10-26 23:40:45 +0200

    Updating the documentation of class Rectangle and class RectangleArea

--- C++/Rectangle Area/Rectangle_Area.cpp
+++ C++/Rectangle Area/Rectangle_Area.cpp
@@ -4,13 +4,27 @@ using namespace std;
 /*
  * Create classes Rectangle and RectangleArea
  */
+
+/*
+ *class Rectangle  
+ * The class should have the following : 
+ *
+ * Member Variables: 
+ * int width // an integer that represents the width of the rectangle
+ * int height // an integer that represents the height of the rectangle
+ *
+ * Member Functions:
+ * void read_input() // Reads the values from the input stream into width and height
+ * void display () // Sends the values of width and height to output stream 
+ *
+*/
 class Rectangle{
     protected:
         int width;
         int height;
 
     public:
-        void read_input(){
+        c read_input(){
             cin >> width;
             cin >> height;
         }
@@ -21,6 +35,17 @@ class Rectangle{
 
 };
 
+/*
+ *class RectangleArea  
+ * The class should have the following : 
+ *
+ * Member Variables: 
+ * int Area // an integer that represents the area of the rectangle
+ *
+ * Member Functions:
+ * void display () // Sends the value of the area to output stream 
+ *
+*/
 class RectangleArea:public Rectangle{
     private:
         int Area;
